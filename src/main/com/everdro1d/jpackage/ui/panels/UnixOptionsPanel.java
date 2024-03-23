9c234704c3ef81b7a8ebeb974a050f3079770467
everdro1d <everdro1d-github@pm.me>
2024-03-22 21:14:28 -0600

    feat: added options to unix panel

--- src/main/com/everdro1d/jpackage/ui/panels/UnixOptionsPanel.java
+++ src/main/com/everdro1d/jpackage/ui/panels/UnixOptionsPanel.java
@@ -1,6 +1,9 @@
 package main.com.everdro1d.jpackage.ui.panels;
 
+import main.com.everdro1d.jpackage.ui.MainWindow;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class UnixOptionsPanel extends JPanel {
     // Unix Options Panel ---------------------------------------------------------------------------------------------|
@@ -23,4 +26,105 @@ public class UnixOptionsPanel extends JPanel {
      * --linux-shortcut
      *   Creates a shortcut for the application.
      */
+    // Variables ------------------------------------------------------------------------------------------------------|
+    private JLabel linuxPackageNameLabel;
+    private JTextField linuxPackageNameTextField;
+    private JLabel linuxDebMaintainerLabel;
+    private JTextField linuxDebMaintainerTextField;
+    private JLabel linuxMenuGroupLabel;
+    private JTextField linuxMenuGroupTextField;
+    private JLabel linuxPackageDepsLabel;
+    private JTextField linuxPackageDepsTextField;
+    private JLabel linuxRpmLicenseTypeLabel;
+    private JTextField linuxRpmLicenseTypeTextField;
+    private JLabel linuxAppReleaseLabel;
+    private JTextField linuxAppReleaseTextField;
+    private JLabel linuxAppCategoryLabel;
+    private JTextField linuxAppCategoryTextField;
+    private JLabel linuxShortcutLabel;
+    private JCheckBox linuxShortcutCheckBox;
+
+    // End of Variables -----------------------------------------------------------------------------------------------|
+
+    // Constructors ---------------------------------------------------------------------------------------------------|
+    public UnixOptionsPanel() {
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.FIRST_LINE_START;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(4, 4, 4, 4);
+        int widthPx = 290;
+
+        // Col 0
+        linuxPackageNameLabel = new JLabel("<html><div style='width:"+widthPx+"px'>Name for Linux package:</div></html>");
+        add(linuxPackageNameLabel, gbc);
+        gbc.gridy++;
+
+        linuxDebMaintainerLabel = new JLabel("<html><div style='width:"+widthPx+"px'>Maintainer for .deb bundle:</div></html>");
+        add(linuxDebMaintainerLabel, gbc);
+        gbc.gridy++;
+
+        linuxMenuGroupLabel = new JLabel("<html><div style='width:"+widthPx+"px'>Menu group this application is placed in:</div></html>");
+        add(linuxMenuGroupLabel, gbc);
+        gbc.gridy++;
+
+        linuxPackageDepsLabel = new JLabel("<html><div style='width:"+widthPx+"px'>Required packages or capabilities for the application:</div></html>");
+        add(linuxPackageDepsLabel, gbc);
+        gbc.gridy++;
+
+        linuxRpmLicenseTypeLabel = new JLabel("<html><div style='width:"+widthPx+"px'>Type of the license:</div></html>");
+        add(linuxRpmLicenseTypeLabel, gbc);
+        gbc.gridy++;
+
+        linuxAppReleaseLabel = new JLabel("<html><div style='width:"+widthPx+"px'>Release value of the RPM <name>.spec file or Debian revision value of the DEB control file:</div></html>");
+        add(linuxAppReleaseLabel, gbc);
+        gbc.gridy++;
+
+        linuxAppCategoryLabel = new JLabel("<html><div style='width:"+widthPx+"px'>Group value of the RPM /.spec file or Section value of DEB control file:</div></html>");
+        add(linuxAppCategoryLabel, gbc);
+        gbc.gridy++;
+
+        gbc.weighty = 1;
+        linuxShortcutLabel = new JLabel("<html><div style='width:"+widthPx+"px'>Creates a shortcut for the application:</div></html>");
+        add(linuxShortcutLabel, gbc);
+
+        // Col 1
+        gbc.gridx++;
+        gbc.gridy = 0;
+        gbc.weightx = 1;
+        gbc.weighty = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        add((linuxPackageNameTextField = new JTextField()), gbc);
+        gbc.gridy++;
+
+        add((linuxDebMaintainerTextField = new JTextField()), gbc);
+        gbc.gridy++;
+
+        add((linuxMenuGroupTextField = new JTextField()), gbc);
+        gbc.gridy++;
+
+        add((linuxPackageDepsTextField = new JTextField()), gbc);
+        gbc.gridy++;
+
+        add((linuxRpmLicenseTypeTextField = new JTextField()), gbc);
+        gbc.gridy++;
+
+        add((linuxAppReleaseTextField = new JTextField()), gbc);
+        gbc.gridy++;
+
+        add((linuxAppCategoryTextField = new JTextField()), gbc);
+        gbc.gridy++;
+
+        gbc.weighty = 1;
+        add((linuxShortcutCheckBox = new JCheckBox()), gbc);
+
+        for (Component component : getComponents()) {
+            if (component.getFont() != null) {
+                component.setFont(new Font(MainWindow.fontName, Font.PLAIN, MainWindow.fontSize));
+            }
+        }
+    }
 }

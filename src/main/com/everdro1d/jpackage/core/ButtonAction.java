60a5d7262ec124a02315af34a7c5c91480e696ad
everdro1d <everdro1d-github@pm.me>
2025-04-19 22:29:57 -0600

    chore [ButtonAction]: add some debug info

--- src/main/com/everdro1d/jpackage/core/ButtonAction.java
+++ src/main/com/everdro1d/jpackage/core/ButtonAction.java
@@ -8,6 +8,8 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
+import static main.com.everdro1d.jpackage.core.MainWorker.debug;
+
 public class ButtonAction {
     private static final String[][] mainSettingKeyMethodPair = new String[][] {
             // MainWindow
@@ -79,17 +81,23 @@ public class ButtonAction {
         getSettingsFromUI();
         if (debug) System.out.println("Saved settings to map.");
         // TODO Save the settings from the commandSettingsMap into a file
+
+        if (debug) System.out.println("Saving settings to file from map.");
     }
 
     public static void loadSettingsFromFile() {
         // TODO open file chooser to load file
         loadSettingsFromFileToMap();
+        if (debug) System.out.println("Loaded settings from file to map.");
         setSettingsFromMap();
+        if (debug) System.out.println("Loaded settings from map to ui.");
     }
 
     public static void assembleAndRunJPackageCommand() {
         // TODO assemble the jpackage command from the commandSettingsMap
         getSettingsFromUI();
+        if (debug) System.out.println("Saved settings from ui to map.");
+
     }
 
     // private methods ------------------------------------------------------------------------------------------------|

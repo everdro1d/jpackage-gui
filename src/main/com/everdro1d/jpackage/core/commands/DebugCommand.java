77ebd1a8dd33cb05f88e259e5b080c41ee6495ea
everdro1d <everdro1d-github@pm.me>
2025-05-08 21:04:55 -1000

    feat [DebugCommand]: add description for debug command

--- src/main/com/everdro1d/jpackage/core/commands/DebugCommand.java
+++ src/main/com/everdro1d/jpackage/core/commands/DebugCommand.java
@@ -5,9 +5,21 @@ import com.everdro1d.libs.commands.CommandManager;
 import main.com.everdro1d.jpackage.core.MainWorker;
 
 public class DebugCommand implements CommandInterface {
+    private String description = "enable debug logging to stdout and launch debug console window";
+
     @Override
     public void execute(CommandManager commandManager) {
         MainWorker.debug = true;
         System.out.println("Debug mode enabled.");
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
